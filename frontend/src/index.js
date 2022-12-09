3369c724fbde3e72d7dd872351006cfd62cf7344
Thanh Tung <thanhtung28400@gmail.com>
2022-12-09 20:00:24 +0700

    feat: add chatbot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,15 @@ import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
 import { ConfigProvider, message } from "antd";
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+Kommunicate.init("25e314a4baf1258d36d5a59e244bf376f", {
+  popupWidget: true,
+  automaticChatOpenOnNavigation: true,
+});
+
 message.config({
   top: 80,
 });
